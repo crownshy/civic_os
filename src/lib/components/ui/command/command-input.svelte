f79da269c919bb88497e0a77760d766799629a0a
Daniela Todorova <43169425+dtodorowa@users.noreply.github.com>
2026-03-16 13:39:26 +0000

    Fix visual bug with combobox

--- src/lib/components/ui/command/command-input.svelte
+++ src/lib/components/ui/command/command-input.svelte
@@ -11,12 +11,12 @@
 	}: CommandPrimitive.InputProps = $props();
 </script>
 
-<div class="flex h-9 items-center gap-2 border-b ps-3 pe-8" data-slot="command-input-wrapper">
+<div class="flex h-9 mt-2 pb-1 items-center gap-2 border-b ps-3 pe-8" data-slot="command-input-wrapper">
 	<SearchIcon class="size-4 shrink-0 opacity-50" />
 	<CommandPrimitive.Input
 		data-slot="command-input"
 		class={cn(
-			"placeholder:text-muted-foreground flex h-10 w-full rounded-md bg-transparent py-3 text-sm outline-hidden disabled:cursor-not-allowed disabled:opacity-50",
+			"border-none bg-transparent placeholder:text-muted-foreground active:border-transparent active:ring-transparent focus:border-transparent focus:ring-transparent flex h-10 w-full rounded-md  py-3 text-sm outline-hidden disabled:cursor-not-allowed disabled:opacity-50 ",
 			className
 		)}
 		bind:ref
