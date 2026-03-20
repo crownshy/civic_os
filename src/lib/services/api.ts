d9e8e2ed18bfa406c07d6781772761be5aadcc7d
Stuart Lynn <stuart.lynn@gmail.com>
2026-03-20 22:47:26 +0000

    Removing destructuring as it was causing docker builds to fail

--- src/lib/services/api.ts
+++ src/lib/services/api.ts
@@ -1,11 +1,11 @@
 import { createApiClient } from '@crownshy/api-client/client';
-import { PUBLIC_CONVERSATION_ID, PUBLIC_INVITE_ID, PUBLIC_POLIS_URL, PUBLIC_POLIS_ID } from '$env/dynamic/public';
+import { env } from '$env/dynamic/public';
 
 export const api = createApiClient('/api', undefined, 'client');
 
 export const config = {
-	conversationId: PUBLIC_CONVERSATION_ID || '',
-	inviteId: PUBLIC_INVITE_ID || '',
-	polisUrl: PUBLIC_POLIS_URL || 'https://polis.comhairle.scot',
-	polisId: PUBLIC_POLIS_ID || '3itaahejzh'
+	conversationId: env.PUBLIC_CONVERSATION_ID || '',
+	inviteId: env.PUBLIC_INVITE_ID || '',
+	polisUrl: env.PUBLIC_POLIS_URL || 'https://polis.comhairle.scot',
+	polisId: env.PUBLIC_POLIS_ID || '3itaahejzh'
 };
