59319a8c603b6595d9fa04deb873b7eff1696e25
Stuart Lynn <stuart.lynn@gmail.com>
2026-04-03 10:37:50 +0100

    Update host name for oregon

--- src/lib/config/regions.ts
+++ src/lib/config/regions.ts
@@ -98,7 +98,7 @@ export const REGIONS: Record<string, RegionConfig> = {
 		polisId: '5v4ictwb87',
 		conversationId: '8a55fb75-5442-4654-886c-339c693b8ac5',
 		inviteId: '9e5e3e93-2a71-4979-bcf4-3c894804fe7f',
-		hostName: 'Oregon Civic Forum',
+		hostName: 'Central Oregon Civic Action Project',
 		hostUrl: '',
 		zipPrefixes: ['97'],
 		slides: [
